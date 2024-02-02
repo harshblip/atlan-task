69aa77b29ffdf1c0feeb5ce3bf453cea30cb144f
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2024-02-02 13:40:03 +0530

    setting up the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ai from '../assets/images/ai.png';
+
+// deferring css. so that html loads first to increase fcp
+
+const Navbar: React.FC = () => {
+    return (
+        <div className='navbar'>
+            
+        </div>
+    );
+};
+
+export default Navbar;
