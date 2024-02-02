0e8f4aebe7de497947ef999d6d4c43232e3b2ea4
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2024-02-03 02:16:30 +0530

    mock api registered

--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+interface Model {
+    id: number;
+    logo: string;
+    modelName: string;
+    name: string;
+    maker: string;
+    accuracy: string;
+    lastUpdated: string;
+    availability: string;
+    description: string;
+    performanceMetrics: string;
+    sampleCode: string;
+    useCases: string;
+    likes: number;
+}
 
 const Discover: React.FC = () => {
+    const [models, setModels] = useState<Model[]>([]);
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/harshblip/mock/models')
+          .then(response => response.json())
+          .then(data => setModels(data))
+          .catch(error => console.error(error));
+     }, []); // passing an empty array so that its called only once
+
+     console.log(models[0].logo);
+
     return (
-        <div>
-            discover
+        <div className="container">
+            <div className="column">Featured</div>
+            <div className="column">
+                <p> AI / LLM Models </p>
+                <div>
+                <img src={models[0].logo} />
+                    {/* <img src = {models[0].logo} alt = "logo" /> */}
+                </div>
+            </div>
         </div>
+
     )
 }
 
