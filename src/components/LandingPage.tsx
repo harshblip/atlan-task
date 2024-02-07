a0d47afa1249d36e63f33c2f2867d7da9eed0004
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2024-02-08 01:43:41 +0530

    faster lcps on pages

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+
 const LandingPage: React.FC = () => {
+    const Container = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    `
+
+    const Text = styled.div`
+        font-weight: 700;
+        font-size: 3.75rem/* 60px */;
+        line-height: 1;
+        margin-top: 6rem;
+        margin-bottom: 6rem;
+    `
+
     const navigate = useNavigate();
     return (
         <div className='bg'>
-            <div className='flex flex-col items-center'>
-                <p className='font-bold text-6xl tagline'> Discover. Explore. Create. </p>
+            <Container>
+                <Text> Discover. Explore. Create. </Text>
                 <button className='btn text-lg font-semibold' onClick={() => navigate('/discover')}>
                         Dive in
                 </button>
-            </div>
+            </Container>
         </div>
     )
 }
