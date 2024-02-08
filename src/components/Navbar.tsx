65c082accf9e2ae7e29de3302d775024a967ec55
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2024-02-08 12:03:28 +0530

    ./

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,17 +9,19 @@ const Navbar: React.FC = () => {
         <>
             <div className='navbar'>
                 <div className='flex'>
-                    <img src={ai} alt='logo' width={40} />
-                    <p className='mt-1 ml-1 text-2xl font-semibold'>AI
-                        <span><code> Playground </code>
+                    <img src={ai} alt='logo' className='h-[2rem] w-[2rem]'/>
+                    <p className='mt-1 ml-1 text-2xl font-semibold flex'>AI
+                        <span><code className='ml-2'> Playground </code>
                         </span>
                     </p>
-                    <div className="blinking-square"></div>
+                    <div className=" blinking-square"></div>
                 </div>
-                <span><p> Features </p></span>
-                <span><p> Github </p></span>
-                <span><p> Home </p></span>
-                <span><p> Contact Us </p></span>
+                <div className='invisible md:visible md:flex md:space-x-8'>
+                    <span><p> Features </p></span>
+                    <span><p> Github </p></span>
+                    <span><p> Home </p></span>
+                    <span><p> Contact Us </p></span>
+                </div>
             </div>
             <LandingPage />
         </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    screens: {
+      'sm': '0px',
+      'md': '730px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
